a1867e18cda04147c6d073fa78cb6dcb76d905bd
Thomas van der Ende <thomas.vd.ende@gmail.com>
2020-06-01 17:09:35 +0200

    correted improt

--- src/app/insects/insects/insects.component.ts
+++ src/app/insects/insects/insects.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { InsectsService } from '../Insects.service';
+import { InsectsService } from '../insects.service';
 import { BehaviorSubject } from 'rxjs';
 import { Insect } from '../insect';
 
